8db2886d51aa2964b41c1d598ca786100a5e1f7f
Eduardo Ferreira de Oliveira <tqi_eoliveira@uolinc.com>
2021-05-20 10:16:41 -0300

    second commit

---
+++ src/main/java/com/example/jenkinsteste/JenkinsTesteApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkinsteste/JenkinsTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
